c08a2327429feba1356d02c21dcb0d6097c1a012
Sonwon112 <tlatmsdkqk@gmail.com>
2023-05-09 21:53:33 +0900

    기본적인 핀 변수 선언

--- arduino_Project.ino
+++ arduino_Project.ino
@@ -2,13 +2,25 @@
 // 로터리 엔코더로 핸들이 어떻게 얼마나 돌아갔는지 감지하여 유니티 프로그램과 시리얼 통신(몇단인지도 송신하여 속도 제어)
 // 버튼(추가구매 필요)을 통해 엑셀과 브레이크 제어
 // 안드로이드와 블루투스 통신으로 현재 몇단인지 전송
-int sonarSensorPin[] = {2,3,4,5,6,7};
+#include <Thread.h>
 
+// 초음파 센서
+int sonarTrig[] = {2,3,4,5,6,7};
+int sonarEcho[] = {8,9,10,11,12,13};
+
+// 로터리 엔코더
+int rotaryCLK = 14;
+int rotaryDT = 15;
+int rotarySW = 16;
+
+// 블루투스 모듈
+int blueTX = 17;
+int blueRX = 18;
 
 
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
