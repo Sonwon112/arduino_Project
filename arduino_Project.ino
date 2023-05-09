2fdaffbedc451282a7544ffd337f0cd1cf4c0f76
Sonwon112 <tlatmsdkqk123@gmail.com>
2023-05-09 21:11:40 +0900

    아두이노 팀프로젝트 아두이노 파일 생성

---
+++ arduino_Project.ino
@@ -0,0 +1,17 @@
+// 초음파 센서로 1단~5단 후진을 감지
+// 로터리 엔코더로 핸들이 어떻게 얼마나 돌아갔는지 감지하여 유니티 프로그램과 시리얼 통신(몇단인지도 송신하여 속도 제어)
+// 버튼(추가구매 필요)을 통해 엑셀과 브레이크 제어
+// 안드로이드와 블루투스 통신으로 현재 몇단인지 전송
+int sonarSensorPin[] = {2,3,4,5,6,7};
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
